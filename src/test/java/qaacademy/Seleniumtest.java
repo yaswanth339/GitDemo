f0e188d18228d8245ea13386fa39cbda2b68483b
yaswanth339 <gyk94944@gmail.com>
2021-02-10 11:04:26 +0530

    this is commit from different times

--- src/test/java/qaacademy/Seleniumtest.java
+++ src/test/java/qaacademy/Seleniumtest.java
@@ -8,12 +8,14 @@ public class Seleniumtest {
   public void BrowserAutomation()
   {
 	System.out.println("brower automation");
+	System.out.println("browser 2 ");
  }
   @Test
   public void testrun()
   {
   
   System.out.println("selenium run");
+  System.out.println("selenium run2");
   }
   
 }
